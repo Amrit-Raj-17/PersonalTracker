fd88a60ac7c99af2605cf9d0f6155b90d89b84f0
Amrit Raj Thakur <amritrajt15@gmail.com>
2026-04-09 13:37:45 +0530

    Added Render deployment files

--- includes/config.php
+++ includes/config.php
@@ -1,8 +1,7 @@
 <?php
-return [
-    'host' => getenv('DB_HOST'),
-    'port' => getenv('DB_PORT'),
-    'dbname' => getenv('DB_NAME'),
-    'user' => getenv('DB_USER'),
-    'password' => getenv('DB_PASS')
-];
+define('DB_HOST', getenv('DB_HOST'));
+define('DB_PORT', getenv('DB_PORT'));
+define('DB_NAME', getenv('DB_NAME'));
+define('DB_USER', getenv('DB_USER'));
+define('DB_PASS', getenv('DB_PASS'));
+?>
